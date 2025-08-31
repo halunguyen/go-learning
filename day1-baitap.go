460df3772440a6bdfe2e3e51b53ea645a6646952
halunguyen <haunp.57@gmail.com>
2025-09-01 00:34:22 +0700

    Day 1 : Bai Tap

---
+++ day1-baitap.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Blockchain with Go!")
+	fmt.Println("My name is Hau Nguyen")
+	fmt.Println("I am learning Go to build Blockchain applications")
+}
